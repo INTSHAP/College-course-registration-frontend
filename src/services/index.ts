f6126078302641f6acd5f4d9c888daa532f8f383
Belloshehu <belloshehu1@gmail.com>
2024-04-20 17:20:06 +0100

    Use production url

--- src/services/index.ts
+++ src/services/index.ts
@@ -7,7 +7,7 @@ const base_api_url_pro = "https://college-course-reg-system.onrender.com/v1";
 // export const LOGIN_URL = base_api_url + "/login";
 // export const SIGNUP_URL = base_api_url + "/signup";
 
-export const API_BASE_URL =
-  NODE_ENV === "production" ? base_api_url_pro : base_api_url_dev;
+export const API_BASE_URL = base_api_url_pro;
+NODE_ENV === "production" ? base_api_url_pro : base_api_url_dev;
 
-export const axiosInstance = axios.create({ baseURL: API_BASE_URL });
+export const axiosInstance = axios.create({ baseURL: base_api_url_pro });
